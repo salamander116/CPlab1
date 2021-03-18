ac2db221c0992b9a0bda70a9a47da366f06efd2c
Miguel Candeias <js.mc.tic@gmai.com>
2021-03-18 18:36:39 +0000

    Concurrent Algorithm Solved
    Still testing several parameters, but looks correct apriori

--- src/MonteSeq.java
+++ src/MonteSeq.java
@@ -1,54 +1,80 @@
 import java.awt.geom.Point2D;
-import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.*;
 
 public class MonteSeq {
 
-    static int min = 0;
-    static int max = 1;
-    public static List<Point2D> points = new LinkedList<Point2D>();
+//    static int min = 0;
+//    static int max = 1;
+    public static ConcurrentHashMap<Point2D, Point2D> points = new ConcurrentHashMap<>();
 
-    private static void shotDarts() {
+    //Not used in Concurrent Implementation
+//    private static void shotDarts() {
+//
+//        Point2D p;
+//
+//        do {
+//            //generates random value and in range
+//            double random = new Random().nextDouble();
+//            double x = min + (random * (max - min));
+//
+//            //generates random value and in range
+//            random = new Random().nextDouble();
+//            double y = min + (random * (max - min));
+//
+//            if (((x * x) + (y * y)) <= 1) {
+//                p = new Point2D.Double(x, y);
+//                if (!points.contains(p)) {
+//                    points.add(p);
+//                } else {
+//                    p = null;
+//                }
+//            } else {
+//                return;
+//            }
+//        } while (p == null);
+//
+//    }
 
-        Point2D p = null;
+    public static void main(String[] args) throws InterruptedException {
 
-        do {
-            //generates random value and in range
-            double random = new Random().nextDouble();
-            double x = min + (random * (max - min));
+        double nShots = Double.parseDouble(args[0]); //Shots Parameter
+        int threadNumber = Integer.parseInt(args[1]); //Thread Parameter
+        int magicnumber = (int) Math.floor(nShots /threadNumber);
+        int sum = 0;
 
-            //generates random value and in range
-            random = new Random().nextDouble();
-            double y = min + (random * (max - min));
 
-            if (((x * x) + (y * y)) <= 1) {
-                p = new Point2D.Double(x, y);
-                if (!points.contains(p)) {
-                    points.add(p);
-                } else {
-                    p = null;
-                }
-            } else {
-                return;
+
+
+
+        Thread[] threads = new Thread[threadNumber];
+
+        for(int i = 0; i< threadNumber; i++){
+            if(i == threadNumber -1){
+               threads[i] = new Worker((int)(nShots - sum),points);
+               threads[i].start();
+            }else{
+                sum += magicnumber;
+                threads[i] = new Worker(magicnumber,points);
+                threads[i].start();
             }
-        } while (p == null);
-
-    }
-
-    public static void main(String[] args) {
-
-        double nShots = Double.parseDouble(args[0]);
-
-        for (int i = 0; i < nShots; i++) {
-            shotDarts();
         }
 
-        double p = (double) points.size() / nShots;
-        double pi = p * 4;
-        System.out.print("Total Number of points: " + nShots + "\n" +
-                "Points within the circle: " + points.size() + "\n" +
-                "Pi estimation: " + pi + "\n");
+        for(int i = 0; i< threadNumber; i++){
+            threads[i].join();
+        }
+
+            double p = (double) points.size() / nShots;
+            double pi = p * 4;
+            System.out.print("Total Number of points: " + nShots + "\n" +
+                    "Points within the circle: " + points.size() + "\n" +
+                    "Pi estimation: " + pi + "\n");
+
+
+
+
     }
 }
---
+++ src/Worker.java
@@ -0,0 +1,46 @@
+import java.awt.geom.Point2D;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Worker extends Thread {
+
+    public ConcurrentHashMap<Point2D, Point2D> sharedPoints;
+    public int nshots;
+
+    static int min = 0;
+    static int max = 1;
+
+    public Worker(int nShots, ConcurrentHashMap<Point2D, Point2D> p) {
+        sharedPoints = p;
+        this.nshots = nShots;
+
+    }
+
+    public void run() {
+
+
+        for (int i = 0; i < nshots; i++) {
+
+            Point2D p = null;
+
+
+            //generates random value and in range
+            double random = new Random().nextDouble();
+            double x = min + (random * (max - min));
+
+            //generates random value and in range
+            random = new Random().nextDouble();
+            double y = min + (random * (max - min));
+
+            if (((x * x) + (y * y)) <= 1) {
+                p = new Point2D.Double(x, y);
+                if (!sharedPoints.containsKey(p)) {
+                    sharedPoints.put(p, p);
+                }
+
+            }
+
+
+        }
+    }
+}
